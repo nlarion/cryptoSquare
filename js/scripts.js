5944517af0d444479f761d30540cea2236ac2642
Neil Larion and Taylor Pokoj <pokojt@gmail.com>
2016-01-19 15:28:04 -0800

    Add test and logic for turning strings into columns for the cryptosquare to consume.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,41 @@
-var cryptoSquare = function(message){
+var stripText = function(message){
   message = message.toLowerCase();
-  message = message.replace(" ","");
+  message = message.replace(/ /g,'');
   message = message.replace(/[^\w\s]/gi, '');
   return message;
+};
 
+var cryptoSquare = function(message){
+  message = stripText(message);
+  message = makeColumn(message);
+  //do something to array to make the coded message.
+
+  return message;
+};
+
+var columnCount =  function(message){
+  //work goes here
+  var length = message.length;
+  var squareRoot = Math.sqrt(length);
+  var column = Math.ceil(squareRoot);
+  return parseInt(column);
+}
+
+var makeColumn = function(message){
+  var arr = [];
+  var str = message.split("");
+  var col = columnCount(message);
+  while ( str.length > 0 ){
+    var combined = "";
+    for(var i = 0; i < col; i++){
+      if(str[0]){
+        combined = combined.concat(str[0]);
+        str.shift();
+      }
+    }
+    arr.push(combined);
+  }
+  return arr;
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,15 +1,30 @@
-describe("cryptoSquare", function(){
+describe("stripText", function(){
 
   it("Makes string lowercase", function(){
-    expect(cryptoSquare("Hi")).to.equal("hi");
+    expect(stripText("Hi")).to.equal("hi");
   });
   it("deletes spaces", function(){
-    expect(cryptoSquare(" ")).to.equal("");
+    expect(stripText(" ")).to.equal("");
   });
   it("deletes special characters", function(){
-    expect(cryptoSquare("don't")).to.equal("dont");
+    expect(stripText("don't")).to.equal("dont");
   });
+});
+
+describe("columnCount", function(){
   it("takes square root of total #characters and rounds up to get the number of colums", function(){
-    expect(cryptoSquare("55")).to.equal("8");
+    expect(columnCount(stripText("This isn't some message"))).to.equal(5);
+  });
+});
+
+describe("makeColumn", function(){
+  it("takes columCount and arranges message into that number of colums", function(){
+    expect(makeColumn(stripText("This isn't some message"))).to.eql(["thisi","sntso","memes","sage"]);
+  });
+});
+
+describe("cryptoSquare", function(){
+  it("reads down columns from left to right and writes characters into groups of 5", function(){
+    expect(cryptoSquare("This isn't some message"))).to.equal("tsmsh neait mgsse eios");
   });
 });
